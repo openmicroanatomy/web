fb131b0e0df53e37a17db3d5cb1fed047bf7df17
Aaron Yli-Hallila <aaron.yli-hallila@student.oulu.fi>
2022-07-20 18:35:15 +0300

    refactor: move viewer logic to separate class

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,13 +1,11 @@
 import { fetchSlide } from "lib/api";
 import { selectedAnnotationState } from "lib/atoms";
-import { area, centroid, clamp } from "lib/helpers";
-import OpenSeadragon from "openseadragon";
+import EduViewer from "lib/EduViewer";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useRecoilState } from "recoil";
 import "styles/Viewer.css";
-import { Annotation, LineString, Polygon } from "types";
-import { sha1 } from "object-hash";
+import { Annotation } from "types";
 
 interface ViewerProps {
     slideId?: string | null;
@@ -16,76 +14,20 @@ interface ViewerProps {
 
 function Viewer({ slideId, annotations }: ViewerProps) {
     const [selectedAnnotation, setSelectedAnnotation] = useRecoilState(selectedAnnotationState);
-    const [viewport, setViewport] = useState<OpenSeadragon.Viewport>();
-    const [overlay, setOverlay] = useState<OpenSeadragon.SvgOverlay>();
-    const [viewer, setViewer] = useState<OpenSeadragon.Viewer | null>(null);
+    const [viewer, setViewer] = useState<EduViewer>();
     const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
-        if (selectedAnnotation) {
-            PanToAnnotation(selectedAnnotation);
-            ZoomToAnnotation(selectedAnnotation);
-            HighlightAnnotation(selectedAnnotation);
+        if (selectedAnnotation && viewer) {
+            viewer.PanToAnnotation(selectedAnnotation);
+            viewer.ZoomToAnnotation(selectedAnnotation);
+            viewer.HighlightAnnotation(selectedAnnotation);
         }
     }, [selectedAnnotation]);
 
-    const PanToAnnotation = (annotation: Annotation) => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const screenWidth = (viewport as any)._contentSize.x;
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const screenHeight = (viewport as any)._contentSize.y;
-
-        const centre = centroid(annotation.geometry, screenWidth, screenHeight);
-        viewport?.panTo(centre);
-    }
-
-    // TODO: This doesn't work on very large or small annotations, but clamping the zoom partially fixes this.
-    const ZoomToAnnotation = (annotation: Annotation) => {
-        if (!viewport) return;
-
-        const annotationArea = area(annotation.geometry);
-        const slideArea = viewport.getContainerSize().x * viewport.getContainerSize().y;
-
-        const MaxZoom = viewport.getMaxZoom();
-        const MinZoom = viewport.getMinZoom();
-        const NewZoom = viewport.imageToViewportZoom(slideArea / annotationArea);
-
-        viewport.zoomTo(clamp(NewZoom, MinZoom, MaxZoom));
-    }
-
-    const HighlightAnnotation = (annotation: Annotation) => {
-        if (!overlay) return;
-
-        const SelectedAnnotationHash = sha1(annotation.geometry.coordinates[0]);
-
-        // First remove any highlight by removing the `selected--annotation` class from every annotation
-        window.d3
-            .select(overlay.node())
-            .selectAll(".annotation")
-            .classed("selected--annotation", false)
-
-        // Now add the `selected--annotation` class to our selected annotation
-        window.d3
-            .select(overlay.node())
-            .selectAll(".annotation")
-            .filter(function(d) { 
-                // Check that we have valid data
-                if (Array.isArray(d)) {
-                    const CurrentAnnotationHash = sha1(d);
-
-                    return SelectedAnnotationHash == CurrentAnnotationHash;
-                } 
-
-                return false 
-            })
-            .classed("selected--annotation", true);
-    };
-
     useEffect(() => {
-        const viewer = window.OpenSeadragon({
+        setViewer(new EduViewer(window.OpenSeadragon({
             id: "Viewer",
-            prefixUrl: "assets/images/",
             defaultZoomLevel: 0,
             //debugMode: process.env.NODE_ENV !== "production",
             showNavigator: true,
@@ -95,188 +37,30 @@ function Viewer({ slideId, annotations }: ViewerProps) {
             gestureSettingsMouse: {
                 clickToZoom: false,
                 dblClickToZoom: true,
-            },
-        });
-
-        setViewer(viewer);
-    }, []);
-
-    useEffect(() => {
-        drawViewer();
-    }, [slideId]);
-
-    useEffect(() => {
-        // Without this the Viewer is not rendered if the user selects a slide prior to opening the Viewer tab, the viewer
-        drawViewer();
-    }, [viewer]);
-
-    function drawViewer() {
-        if (!slideId || !viewer || !annotations) {
-            return;
-        }
-
-        // TODO: Refactor this to its own class/functions etc.
-        const apiHelper = async () => {
-            const result = await fetchSlide(slideId);
-
-            const downsamples: number[] = [];
-            const levelCount = parseInt(result["openslide.level-count"]);
-
-            const slideHeight = parseInt(result["openslide.level[0].height"]);
-            const slideWidth = parseInt(result["openslide.level[0].width"]);
-
-            const tileHeight = parseInt(result["openslide.level[0].tile-width"]);
-            const tileWidth = parseInt(result["openslide.level[0].tile-height"]);
-
-            for (let i = 0; i < levelCount; i++) {
-                downsamples.push(Math.floor(parseInt(result["openslide.level[" + i + "].downsample"])));
             }
+        }), setSelectedAnnotation));
+    }, [])
 
-            viewer.open({
-                height: slideHeight,
-                width: slideWidth,
-                tileHeight: tileHeight,
-                tileWidth: tileWidth,
-                minLevel: 0,
-                maxLevel: levelCount - 1,
-                getLevelScale: function (level: number) {
-                    return 1 / downsamples[levelCount - level - 1];
-                },
-                getTileUrl: function (level: number, x: number, y: number) {
-                    level = levelCount - level - 1;
+    useEffect(() => {
+        if (!slideId) return;
 
-                    const downsample = downsamples[level];
+        fetchSlide(slideId)
+            .then((data) => {
+                viewer?.LoadSlide(data);
 
-                    let adjustY = 0;
-                    let adjustX = 0;
+                viewer?.ClearAnnotations();
 
-                    const tileY = y * tileHeight * downsample;
-                    const tileX = x * tileWidth * downsample;
-
-                    if (tileX + downsample * tileWidth > slideWidth) {
-                        adjustX = tileWidth - Math.floor(Math.abs((tileX - slideWidth) / downsample));
-                    }
-
-                    if (tileY + downsample * tileHeight > slideHeight) {
-                        adjustY = tileHeight - Math.floor(Math.abs((tileY - slideHeight) / downsample));
-                    }
-
-                    const height = tileHeight - adjustY;
-                    const width = tileWidth - adjustX;
-
-                    return result["openslide.remoteserver.uri"]
-                        .replace("{tileX}", String(tileX as unknown))
-                        .replace("{tileY}", String(tileY as unknown))
-                        .replace("{level}", String(level as unknown))
-                        .replace("{tileWidth}", String(width as unknown))
-                        .replace("{tileHeight}", String(height as unknown));
-                },
-            });
-
-            // This could also be openslide.mpp-x or openslide.mpp-y
-            // TODO: Create fallback aperio.MPP does not exist
-            const mpp = parseFloat(result["aperio.MPP"]);
-
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            viewer.scalebar({
-                xOffset: 10,
-                yOffset: 10,
-                barThickness: 3,
-                color: "#555555",
-                fontColor: "#333333",
-                backgroundColor: "rgba(255, 255, 255, 0.5)",
-                pixelsPerMeter: mpp ? 1e6 / mpp : 0,
-            });
-
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            const overlay = viewer.svgOverlay() as SvgOverlay;
-
-            // Clear any annotations from a previous slide
-            window.d3.select(overlay.node()).selectAll("*").remove();
-
-            Array.from(annotations).forEach((annotation) => {
-                if (annotation.geometry.type === "LineString") {
-                    drawLine(annotation);
-                } else if (annotation.geometry.type === "Polygon") {
-                    drawPolygon(annotation);
-                } else {
-                    console.log(`${annotation.geometry.type} geometry type not implemented.`);
+                if (annotations) {
+                    viewer?.DrawAnnotations(annotations);
                 }
+            }).catch((error) => {
+                setError(error);
+                toast.error(error.message);
+                console.error(error);
             });
-
-            function drawLine(annotation: Annotation) {
-                const coordinates = annotation.geometry.coordinates as LineString;
-
-                window.d3
-                    .select(overlay.node())
-                    .append("line")
-                    .attr("id", sha1(coordinates))
-                    .attr("class", "annotation")
-                    .data(coordinates)
-                    .style("stroke", "#f00")
-                    .attr("x1", scaleX(coordinates[0][0]))
-                    .attr("y1", scaleY(coordinates[0][1]))
-                    .attr("x2", scaleX(coordinates[1][0]))
-                    .attr("y2", scaleY(coordinates[1][1]))
-                    .on("click", () => { setSelectedAnnotation(annotation) });
-            }
-
-            function drawPolygon(annotation: Annotation) {
-                const coordinates = annotation.geometry.coordinates as Polygon;
-                
-                window.d3
-                    .select(overlay.node())
-                    .append("polygon")
-                    .attr("id", sha1(coordinates))
-                    .attr("class", "annotation")
-                    .data(coordinates)
-                    .style("stroke", "#f00")
-                    .style("fill", "transparent")
-                    .attr("points", function () {
-                        return coordinates[0]
-                            .map(function (point: number[]) {
-                                return [scaleX(point[0]), scaleY(point[1])].join(",");
-                            })
-                            .join(" ");
-                    })
-                    .on("click", () => { setSelectedAnnotation(annotation) });
-            }
-
-            function scaleX(x: number) {
-                return x / slideWidth;
-            }
-
-            function scaleY(y: number) {
-                // For some reason we need to multiply the y-coordinate by the ratio of the height and width.
-                // This is not necessary for the x-coordinate for some reason.
-                // This requires some further investigation into why this happens.
-                return (y / slideHeight) * (slideHeight / slideWidth);
-            }
-
-            // 0.001 is the default size for stroke thickness as defined in Viewer.css
-            const ScalingFactor = 0.001 / viewer.viewport.getMinZoom();
-
-            viewer.addHandler('zoom', function (viewer) {
-                // Thickness = ScalingFactor * Inverse Zoom
-                const thickness = ScalingFactor * (1 / (viewer.zoom ?? 1));
-                document.documentElement.style.setProperty(`--stroke-thickness`, String(thickness));
-            })
-
-            overlay.resize();
-            setViewport(viewer.viewport);
-            setOverlay(overlay);
-        };
-
-        apiHelper().catch((e) => {
-            setViewer(null);
-            if (e instanceof Error) {
-                setError(e);
-                toast.error(e.message);
-            }
-        });
-    }
+    }, [slideId, viewer]);
+    // This hook needs to also run when Viewer changes because the setViewer() function is
+    // async and has not finished unless the user has already opened the Viewer tab.
 
     if (error) {
         return <p>Unexpected error with Viewer</p>;
---
+++ src/lib/EduViewer.ts
@@ -0,0 +1,246 @@
+import { SvgOverlay } from "openseadragon";
+import { Annotation, LineString, Polygon } from "types";
+import { sha1 } from "object-hash";
+import { area, centroid, clamp } from "./helpers";
+import { SetterOrUpdater } from "recoil";
+
+export default class EduViewer {
+
+    private Viewer: OpenSeadragon.Viewer;
+    private Overlay!: OpenSeadragon.SvgOverlay;
+
+    private Downsamples!: number[];
+    private LevelCount!: number;
+
+    private SlideHeight!: number;
+    private SlideWidth!: number;
+
+    private TileHeight!: number;
+    private TileWidth!: number;
+    
+    private ServerUri!: string;
+    private MillimetersPerPixel!: number;
+
+    private SetSelectedAnnotation: SetterOrUpdater<Annotation | null>;
+
+    constructor(viewer: OpenSeadragon.Viewer, updater: SetterOrUpdater<Annotation | null>) {
+        this.Viewer = viewer;
+        this.SetSelectedAnnotation = updater;
+    }
+
+    LoadSlide(data: Record<string, string>) {
+        this.LevelCount = parseInt(data["openslide.level-count"]);
+
+        this.Downsamples = [];
+        for (let i = 0; i < this.LevelCount; i++) {
+            const downsample = parseInt(data["openslide.level[" + i + "].downsample"]);
+            this.Downsamples.push(Math.floor(downsample));
+        }
+
+        this.SlideHeight = parseInt(data["openslide.level[0].height"]);
+        this.SlideWidth  = parseInt(data["openslide.level[0].width"]);
+        this.TileHeight  = parseInt(data["openslide.level[0].tile-width"]);
+        this.TileWidth   = parseInt(data["openslide.level[0].tile-height"]);
+        this.ServerUri   = data["openslide.remoteserver.uri"];
+
+        // TODO: Create fallback if doesn't exist (alternatively openslide.mpp-x or openslide.mpp-y)
+        this.MillimetersPerPixel = parseFloat(data["aperio.MPP"])
+
+        this.InitializeViewer();
+        this.InitializeScalebar();
+        this.InitializeOverlay();
+
+        this.RegisterOnZoomListener();
+    }
+
+    InitializeViewer() {
+        this.Viewer.open({
+            height: this.SlideHeight,
+            width: this.SlideWidth,
+            tileHeight: this.TileHeight,
+            tileWidth: this.TileWidth,
+            minLevel: 0,
+            maxLevel: this.LevelCount - 1,
+            tileOverlap: 0,
+            getLevelScale: (level: number) => {
+                return 1 / this.Downsamples[this.LevelCount - level - 1];
+            },
+            getTileUrl: (level: number, x: number, y: number) => {
+                level = this.LevelCount - level - 1;
+
+                const downsample = this.Downsamples[level];
+
+                let adjustY = 0;
+                let adjustX = 0;
+
+                const tileY = y * this.TileHeight * downsample;
+                const tileX = x * this.TileWidth  * downsample;
+
+                if (tileX + downsample * this.TileWidth > this.SlideWidth) {
+                    adjustX = this.TileWidth  - Math.floor(Math.abs((tileX - this.SlideWidth) / downsample));
+                }
+
+                if (tileY + downsample * this.TileHeight > this.SlideHeight) {
+                    adjustY = this.TileHeight - Math.floor(Math.abs((tileY - this.SlideHeight) / downsample));
+                }
+
+                const height = this.TileHeight - adjustY;
+                const width  = this.TileWidth  - adjustX;
+
+                return this.ServerUri
+                           .replace("{tileX}", String(tileX))
+                           .replace("{tileY}", String(tileY))
+                           .replace("{level}", String(level))
+                           .replace("{tileWidth}", String(width))
+                           .replace("{tileHeight}", String(height));
+            },
+        });
+    }
+
+    InitializeScalebar() {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.Viewer.scalebar({
+            xOffset: 10,
+            yOffset: 10,
+            barThickness: 3,
+            color: "#555555",
+            fontColor: "#333333",
+            backgroundColor: "rgba(255, 255, 255, 0.5)",
+            pixelsPerMeter: this.MillimetersPerPixel ? 1e6 / this.MillimetersPerPixel : 0,
+        });
+    }
+
+    InitializeOverlay() {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.Overlay = this.Viewer.svgOverlay() as SvgOverlay;
+    }
+
+    RegisterOnZoomListener() {
+        // 0.001 is the default size for stroke thickness as defined in Viewer.css
+        const ScalingFactor = 0.001 / this.Viewer.viewport.getMinZoom();
+
+        this.Viewer.addHandler('zoom', function (viewer) {
+            // Thickness = ScalingFactor * Inverse Zoom
+            const thickness = ScalingFactor * (1 / (viewer.zoom ?? 1));
+            document.documentElement.style.setProperty(`--stroke-thickness`, String(thickness));
+        });
+    }
+    
+    ClearAnnotations() {
+        window.d3
+            .select(this.Overlay.node())
+            .selectAll("*")
+            .remove();
+    }
+
+    PanToAnnotation(annotation: Annotation) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const screenWidth = (this.Viewer.viewport as any)._contentSize.x;
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const screenHeight = (this.Viewer.viewport as any)._contentSize.y;
+
+        const centre = centroid(annotation.geometry, screenWidth, screenHeight);
+        this.Viewer.viewport.panTo(centre);
+    }
+    
+    ZoomToAnnotation(annotation: Annotation) {
+        // TODO: This doesn't work on very large or small annotations, but clamping the zoom partially fixes this.
+        const annotationArea = area(annotation.geometry);
+        const slideArea = this.Viewer.viewport.getContainerSize().x * this.Viewer.viewport.getContainerSize().y;
+
+        const MaxZoom = this.Viewer.viewport.getMaxZoom();
+        const MinZoom = this.Viewer.viewport.getMinZoom();
+        const NewZoom = this.Viewer.viewport.imageToViewportZoom(slideArea / annotationArea);
+        this.Viewer.viewport.zoomTo(clamp(NewZoom, MinZoom, MaxZoom));
+    }
+
+    HighlightAnnotation(annotation: Annotation) {
+        const SelectedAnnotationHash = sha1(annotation.geometry.coordinates[0]);
+
+        // First remove any highlight by removing the `selected--annotation` class from every annotation
+        window.d3
+            .select(this.Overlay.node())
+            .selectAll(".annotation")
+            .classed("selected--annotation", false)
+
+        // Now add the `selected--annotation` class to our selected annotation
+        window.d3
+            .select(this.Overlay.node())
+            .selectAll(".annotation")
+            .filter(function(d) { 
+                // Check that we have valid data
+                if (Array.isArray(d)) {
+                    const CurrentAnnotationHash = sha1(d);
+
+                    return SelectedAnnotationHash == CurrentAnnotationHash;
+                } 
+
+                return false 
+            })
+            .classed("selected--annotation", true);
+    }
+
+    DrawAnnotations(annotations: Annotation[]) {
+        Array.from(annotations).forEach((annotation) => {
+            if (annotation.geometry.type === "LineString") {
+                this.DrawLine(annotation);
+            } else if (annotation.geometry.type === "Polygon") {
+                this.DrawPolygon(annotation);
+            } else {
+                console.log(`${annotation.geometry.type} geometry type not implemented.`);
+            }
+        });
+    }
+
+    private DrawLine(annotation: Annotation) {
+        const coordinates = annotation.geometry.coordinates as LineString;
+
+        window.d3
+            .select(this.Overlay.node())
+            .append("line")
+            .attr("id", sha1(coordinates))
+            .attr("class", "annotation")
+            .data(coordinates)
+            .style("stroke", "#f00")
+            .attr("x1", this.ScaleX(coordinates[0][0]))
+            .attr("y1", this.ScaleY(coordinates[0][1]))
+            .attr("x2", this.ScaleX(coordinates[1][0]))
+            .attr("y2", this.ScaleY(coordinates[1][1]))
+            .on("click", () => { this?.SetSelectedAnnotation(annotation) });
+    }
+
+    private DrawPolygon(annotation: Annotation) {
+        const coordinates = annotation.geometry.coordinates as Polygon;
+                
+        window.d3
+            .select(this.Overlay.node())
+            .append("polygon")
+            .attr("id", sha1(coordinates))
+            .attr("class", "annotation")
+            .data(coordinates)
+            .style("stroke", "#f00")
+            .style("fill", "transparent")
+            .attr("points", () => {
+                return coordinates[0]
+                    .map((point: number[]) => {
+                        return [this.ScaleX(point[0]), this.ScaleY(point[1])].join(",");
+                    })
+                    .join(" ");
+            })
+            .on("click", () => { this?.SetSelectedAnnotation(annotation) });
+    }
+
+    private ScaleX(x: number) {
+        return x / this.SlideWidth;
+    }
+
+    private ScaleY(y: number) {
+        // For some reason we need to multiply the y-coordinate by the ratio of the height and width.
+        // This is not necessary for the x-coordinate for some reason.
+        // This requires some further investigation into why this happens.
+        return (y / this.SlideHeight) * (this.SlideHeight / this.SlideWidth);
+    }
+}
