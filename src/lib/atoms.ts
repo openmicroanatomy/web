7cc318cd96de2d87137b543e1c054620ee866cf0
Aaron Yli-Hallila <aaron.yli-hallila@student.oulu.fi>
2023-11-25 10:21:17 +0200

    refactor: remove unused atoms

--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -12,19 +12,6 @@ export const selectedAnnotationState = atom({
     default: null as Annotation | null,
 });
 
-export const viewportState = atom({
-    key: "viewportState",
-    default: null as OpenSeadragon.Viewport | null,
-    dangerouslyAllowMutability: true,
-});
-
-export const overlayState = atom({
-    key: "overlayState",
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    default: null as any,
-    dangerouslyAllowMutability: true,
-});
-
 export const currentSlideState = atom({
     key: "currentSlide",
     default: null as Slide | null
